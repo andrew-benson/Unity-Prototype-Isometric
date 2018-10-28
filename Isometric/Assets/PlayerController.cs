a45a6dd7bcdd4cd241ff1c3ff6aabb6f40d5c1c5
andrew-benson <andrew.benson@grahambrown.com>
2018-10-28 18:04:55 +0000

    Mapped PS4 controller to input buttons

---
+++ Isometric/Assets/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public float PlayerSpeed = 5f;
+
+    float sideMovement, forwardMovement;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        sideMovement += PlayerSpeed * Time.deltaTime * Input.GetAxis("Horizontal");
+        forwardMovement += PlayerSpeed * Time.deltaTime * Input.GetAxis("Vertical");
+        transform.position = new Vector3(sideMovement, transform.position.y, forwardMovement);
+        
+
+        Debug.Log("MOUSE X: " + Input.GetAxis("Mouse X"));
+        Debug.Log("MOUSE Y " + Input.GetAxis("Mouse Y"));
+
+        if(Input.GetAxis("Jump") > 0)
+        {
+            Debug.Log("Pressed Jump");
+        }
+    }
+}
