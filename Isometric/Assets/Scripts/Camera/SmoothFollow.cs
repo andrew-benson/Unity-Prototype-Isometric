477a354df4d27a6621b5667f96e09627fd1bffb9
andrew-benson <andrew.benson@grahambrown.com>
2018-10-28 18:56:31 +0000

    progress on camera

---
+++ Isometric/Assets/Scripts/Camera/SmoothFollow.cs
@@ -0,0 +1,50 @@
+﻿// Smooth Follow from Standard Assets
+// Converted to C# because I fucking hate UnityScript and it's inexistant C# interoperability
+// If you have C# code and you want to edit SmoothFollow's vars ingame, use this instead.
+using UnityEngine;
+using System.Collections;
+
+public class SmoothFollow : MonoBehaviour
+{
+
+    // The target we are following
+    public Transform target;
+
+    // the height we want the camera to be above the target
+    public float height = 5.0f;
+    public float zDistance = 8f;
+    // How much we 
+    public float heightDamping = 2.0f;
+    public float rotationDamping = 3.0f;
+
+    // Place the script in the Camera-Control group in the component menu
+    [AddComponentMenu("Camera-Control/Smooth Follow")]
+
+    void LateUpdate()
+    {
+        // Early out if we don't have a target
+        if (!target) return;
+
+        // Calculate the current rotation angles
+        float wantedHeight = target.position.y + height;
+        float wantedDistance = transform.position.z - zDistance;
+
+        float currentHeight = transform.position.y;
+        //float currentDistanceZ = transform.position.z;
+        //float currentDistanceZ = transform.position.z;
+
+        // Damp the height
+        currentHeight = Mathf.Lerp(currentHeight, wantedHeight, heightDamping * Time.deltaTime);
+        //currentDistanceZ = Mathf.Lerp(currentDistanceZ, wantedDistance, heightDamping * Time.deltaTime);
+
+        // Set the position of the camera on the x-z plane to:
+        // distance meters behind the target
+        transform.position = target.position;
+
+        // Set the height of the camera
+        transform.position = new Vector3(transform.position.x, currentHeight, transform.position.z - zDistance);
+
+        // Always look at the target
+        //transform.LookAt(target);
+    }
+}
