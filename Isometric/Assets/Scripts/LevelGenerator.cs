2ca9cbac87ef307e4f601abae0827a31619b4238
andrew-benson <andrew.benson@grahambrown.com>
2018-11-01 14:08:09 +0000

    Added simple level generator

---
+++ Isometric/Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour {
+	public Texture2D levelImage;
+	// Use this for initialization
+	void Awake () {
+
+		var pixelCountWidth = levelImage.width;
+		var pixelCountHeight = levelImage.height;
+
+		for (int x = 0; x < pixelCountWidth; x++)
+		{
+			for (int y = 0; y < pixelCountHeight; y++)
+			{
+				var pixelColour = levelImage.GetPixel(x, y);
+					Debug.Log("Pixel at " + x.ToString() +"," + y.ToString() + " colour: " + pixelColour.ToString() );
+
+				if (pixelColour == Color.black)
+				{
+					GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+					cube.transform.position = new Vector3(x, 0, y);
+					cube.transform.parent = this.transform;
+					Debug.Log("Found a black pixel");
+				}
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
