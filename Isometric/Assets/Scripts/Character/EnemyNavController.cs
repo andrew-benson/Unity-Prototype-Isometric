b5bee3f1f3b23fa86d492eeddd7b0a3a88c84382
andrew-benson <andrew.benson@grahambrown.com>
2019-05-27 15:09:44 +0100

    Enemy with humanoid movement, using navmesh and player as the target

---
+++ Isometric/Assets/Scripts/Character/EnemyNavController.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+public class EnemyNavController : MonoBehaviour
+{
+    public NavMeshAgent agent;
+
+    public ThirdPersonCharacter charactercontroller;
+
+    public Transform target;
+    private bool isReached;
+
+    private void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        charactercontroller = GetComponent<ThirdPersonCharacter>();
+    }
+
+    private void Start()
+    {
+        agent.updateRotation = false;
+        Invoke(nameof(FindPlayer), 0.1f);
+    }
+
+    private void FindPlayer()
+    {
+        target = GameObject.FindWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //MouseClickToFindPlayer();
+        if(target)
+        {
+
+            Debug.Log("Remaining: " + agent.remainingDistance);
+            Debug.Log("stoppingDistance: " + agent.stoppingDistance);
+
+            if (!isReached)
+                agent.SetDestination(target.position);
+
+            if (agent.remainingDistance > agent.stoppingDistance)
+            {
+                charactercontroller.Move(agent.desiredVelocity, false, false);
+            }
+            else
+            {
+                // We've reached the destination
+                charactercontroller.Move(Vector3.zero, false, false);
+
+                if(!agent.pathPending)
+                {
+                    Debug.Log("Reached Destination");
+                    isReached = true;
+                    agent.isStopped = true;
+                }
+            }
+        }
+    }
+
+
+
+    private void MouseClickToFindPlayer()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                agent.SetDestination(target.position);
+                Debug.Log("Moving To: " + agent.destination);
+            }
+            //Debug.Log("Remaining: " + agent.remainingDistance);
+            //Debug.Log("stoppingDistance: " + agent.stoppingDistance);
+        }
+    }
+}
---
+++ Isometric/Assets/Scripts/Character/PlayerController.cs
@@ -0,0 +1,97 @@
+﻿using Assets.Scripts.Constants;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+    #region Gameplay Variables
+    public float PlayerSpeed;
+    public float gravity = -7;
+    public float playerVelocity;
+    public float maxNeutralVelocity = 10;
+    public float maxAimVelocity = 8;
+    private Rigidbody playerRigidbody;
+    #endregion
+
+    [Space(10)]
+
+    #region Input Variables 
+    public float leftX;
+    public float leftY;
+    public float rightX;
+    public float rightY;
+    private Vector2 L_ThumbstickVector;
+    private Vector2 R_ThumbstickVector;
+    #endregion
+
+    private Animator animator;
+
+    private void Awake()
+    {
+        playerRigidbody = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
+    }
+
+    private void FixedUpdate()
+    {
+        LeftThumbStick();
+        RightThumbStick();
+
+        OnScreenDebug();
+    }
+
+    private void OnScreenDebug()
+    {
+        Debug.DrawLine(transform.position, transform.position + new Vector3(L_ThumbstickVector.x,0,L_ThumbstickVector.y), Color.cyan);
+        Debug.DrawLine(transform.position, transform.position + new Vector3(R_ThumbstickVector.x, 0, R_ThumbstickVector.y), Color.red);
+    }
+
+    private void LeftThumbStick()
+    {
+        leftX = Input.GetAxis(InputPresets.HORIZONTAL);
+        leftY = Input.GetAxis(InputPresets.VERTICAL);
+        L_ThumbstickVector = new Vector2(leftX, leftY);
+
+        // Get relationship between the thumbsticks to determine sideways movement
+        var dotProduct = (Vector2.Dot(R_ThumbstickVector, Vector2.Perpendicular(L_ThumbstickVector)));
+
+        // Get cross product for back and forth
+        var signedThumbstickAngle = (Vector2.Dot(R_ThumbstickVector, L_ThumbstickVector));
+
+        if (L_ThumbstickVector != Vector2.zero)
+        {   
+            // Set velocity of rigid body, which is scaled by player speed
+            playerRigidbody.velocity = new Vector3(leftX * PlayerSpeed, playerRigidbody.velocity.y, leftY * PlayerSpeed);
+            playerVelocity = new Vector3(leftX * PlayerSpeed, playerRigidbody.velocity.y, leftY * PlayerSpeed).magnitude;
+
+            animator.SetBool(AnimationParams.ISMOVINGLEFTSTICK, true);
+            animator.SetFloat(AnimationParams.SPEED, playerVelocity);
+            animator.SetFloat(AnimationParams.LEFT_RIGHT, dotProduct);
+            animator.SetFloat(AnimationParams.FRONT_BACK, signedThumbstickAngle);        
+        }
+        else
+            animator.SetBool(AnimationParams.ISMOVINGLEFTSTICK, false);
+
+    }
+
+    private void RightThumbStick()
+    {
+        rightX = Input.GetAxis(InputPresets.MOUSE_X);
+        rightY = Input.GetAxis(InputPresets.MOUSE_Y);
+        R_ThumbstickVector = new Vector2(rightX, rightY);
+
+        var isAiming = R_ThumbstickVector != Vector2.zero;
+        var lookDirection = isAiming ? new Vector3(rightX, 0, rightY) : new Vector3(leftX, 0, leftY);
+        PlayerSpeed = isAiming && lookDirection != Vector3.zero ? maxAimVelocity : maxNeutralVelocity;
+
+        // Rotate only when direction isn't zero
+        if(lookDirection != default)
+            playerRigidbody.MoveRotation(Quaternion.LookRotation(lookDirection));
+
+        // Enables transition in blend tree
+        animator.SetBool(AnimationParams.ISMOVINGRIGHTSTICK, isAiming);
+    }
+}
